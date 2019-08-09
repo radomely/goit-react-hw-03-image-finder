cdbb3e78d0f58922cb1406b3cda1f630f81b4254
Andrii <radomely@gmail.com>
2019-08-09 18:48:17 +0300

    Updates

---
+++ precache-manifest.31ba1a5b2e2174f68ee481a808a934fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a028f27f81a692b591a48eed66396ac0",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "4a8fad3449b388da37a7",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.198934d9.chunk.css"
+  },
+  {
+    "revision": "8e29cd12112615e4513c",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.6d45019f.chunk.css"
+  },
+  {
+    "revision": "4a8fad3449b388da37a7",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.928b18cf.chunk.js"
+  },
+  {
+    "revision": "8e29cd12112615e4513c",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.907d2033.chunk.js"
+  },
+  {
+    "revision": "052338c15f0cfa810fb3",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime~main.a33a0e9d.js"
+  }
+]);
